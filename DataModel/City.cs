2f51fd0953373d84b37f569f7d2516d0cabfd75f
Lilmaniacloco <maradiaga.derick@gmal.com>
2024-09-27 17:46:15 -0700

    Add project files.

---
+++ DataModel/City.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataModel;
+
+public partial class City
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("countryid")]
+    public int Countryid { get; set; }
+
+    [Column("city")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string City1 { get; set; } = null!;
+
+    [Column("lat")]
+    public double Lat { get; set; }
+
+    [Column("lng")]
+    public double Lng { get; set; }
+
+    [Column("population")]
+    public int Population { get; set; }
+
+    [ForeignKey("Countryid")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
